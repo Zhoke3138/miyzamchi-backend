d1ee72264d715cb33a254c5bf22ab9469b2192c6
Zhanybek Asirov <zhasirov@gmail.com>
2026-06-01 19:34:34 +0600

    🛠️ UX/UI fixes + Full Trace Logger implementation

--- server.js
+++ server.js
@@ -3818,7 +3818,7 @@ function getGeminiSetupMessage(modelName) {
                 speechConfig: {
                     voiceConfig: {
                         prebuiltVoiceConfig: {
-                            voiceName: 'Charon'
+                            voiceName: 'Algieba'
                         }
                     }
                 }
